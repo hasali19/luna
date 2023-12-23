0bab322ed74373fb2a086e3525a1ee25ea4c5c3b
Hasan Mohsin <hasan@hasali.dev>
2023-12-23 22:27:13 +0000

    Dark mode splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,6 +95,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.work:work-runtime-ktx:2.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
 
     implementation("androidx.lifecycle:lifecycle-process:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
